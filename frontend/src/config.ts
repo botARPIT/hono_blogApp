117c67133845ee1a91e59863b521c84e8b7511e9
botARPIT <arpitchauhanofficial>
2025-12-22 23:25:31 +0530

    Prod test

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 
-export const BACKEND_URL = "http://localhost:8787"
+export const BACKEND_URL = "https://backend.fevoro2226.workers.dev"
 // export const BACKEND_URL = " https://backend.fevoro2226.workers.dev"
 export const GOOGLE_REDIRECT_URI = `${BACKEND_URL}/api/v1/auth/callback/google`
